ff2461d6c4d954bdcc7c7bf5a2f9aca80686d585
Janos Elek <janos.elek@intec.ugent.be>
2017-04-18 00:09:00 +0200

    Create output dirs for kvstore

--- NFV_LTE_EPC-1.1/scripts/install.sh
+++ NFV_LTE_EPC-1.1/scripts/install.sh
@@ -2,7 +2,7 @@
 
 sudo apt-get update
 sudo apt-get install -y openvpn libsctp-dev openssl
-sudo add-apt-repository "ppa:patrickdk/general-lucid"
+sudo add-apt-repository -y "ppa:patrickdk/general-lucid"
 sudo apt-get update
 sudo apt-get install -y iperf3 iperf htop ipvsadm git libssl-dev g++ libboost-all-dev
 cd kvstore/Implementation/LevelDB_Disk/server
