ee7ae5132d1ff9b1b79355c9a058903c86be5d27
Janos Elek <janos.elek@intec.ugent.be>
2017-04-18 23:53:48 +0200

    Pass env variables to install sciprt

--- NFV_LTE_EPC-1.1/scripts/install.sh
+++ NFV_LTE_EPC-1.1/scripts/install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get install -y openvpn libsctp-dev openssl
-sudo add-apt-repository -y "ppa:patrickdk/general-lucid"
-sudo apt-get update
-sudo apt-get install -y iperf3 iperf htop ipvsadm git libssl-dev g++ libboost-all-dev
+sudo -E apt-get update
+sudo -E apt-get install -q -y openvpn libsctp-dev openssl
+sudo -E add-apt-repository -y "ppa:patrickdk/general-lucid"
+sudo -E apt-get update
+sudo -E apt-get install -q -y iperf3 iperf htop ipvsadm git libssl-dev g++ libboost-all-dev
 cd kvstore/Implementation/LevelDB_Disk/server
 sudo bash install_server.sh
 cd ../client
